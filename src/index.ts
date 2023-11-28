8948e5fc778bca9728abcfc62c06bcae9ce5fa1a
Darren Arbon <35709034+darrenarbon@users.noreply.github.com>
2023-11-28 15:48:42 +0000

    Add sensor information

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,122 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {CSSResultGroup, html, LitElement} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import {HomeAssistant} from 'custom-card-helpers';
-import {styles} from './style';
+import { CSSResultGroup, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { HomeAssistant } from 'custom-card-helpers';
+import { styles } from './style';
 
 @customElement('hive-heating-stats-card')
 export class HiveHeatingStatsCard extends LitElement {
-    @property() public hass!: HomeAssistant;
-    
-    static get styles(): CSSResultGroup {
-        return styles;
-    }
-    
-    static getStubConfig() {
-        return {
-           
-        }
-    }
+	@property() public hass!: HomeAssistant;
 
-    render() {
-        return html`
-            <ha-card>
-                <style>
-                    h1 {
-                        text-align: center;
-                    }
-                </style>
+	static get styles(): CSSResultGroup {
+		return styles;
+	}
+
+	static getStubConfig() {
+		return {};
+	}
+
+	getState(entity: string, defaultValue?: any) {
+		const state = this.hass.states[entity];
+		return state !== undefined ? state : defaultValue;
+	}
+
+	render() {
+		const sensorInformation = this.getState('sensor.heating_on_today');
+		return html`
+            <div class="ha-card">
                 <div class="container card">
                     <h1>Heating History</h1>
+
+                    <div class="grey-box">
+                        <div class="grey-box-half">
+                            Total
+                            <div class="grey-box-units"><span>30</span>h <span>29</span>m</div>
+                        </div>
+                        <div class="grey-box-half">
+                            Avg per day
+                            <div class="grey-box-units"><span>5</span>h <span>12</span>m</div>
+                        </div>
+                    </div>
+                    <br />
+                    <table class="week-view">
+                        <head>
+                            <tr>
+                                <th class="week-view-day-title">Day</th>
+                                <th class="week-view-day-value">Time</th>
+                                <th class="week-view-day-temperatures">Min Max</th>
+                            </tr>
+                        </head>
+                        <tr>
+                            <td class="week-view-day-title">Today</td>
+                            <td class="week-view-day-value">
+                                <div class="week-view-day-value-block MAX-WIDTH-IS-80" style="width: 80%">&nbsp;</div>
+                                <div>&nbsp; 3h 04m</div>
+                            </td>
+                            <td class="week-view-day-temperatures"><div>-2&deg; &nbsp; 2&deg;</div></td>
+                        </tr>
+                        <tr>
+                            <td class="week-view-day-title">Thu 22</td>
+                            <td class="week-view-day-value">
+                                <div class="week-view-day-value-block" style="width: 40%">&nbsp;</div>
+                                <div>&nbsp; 1h 34m</div>
+                            </td>
+                            <td class="week-view-day-temperatures"><div>1&deg; &nbsp; 12&deg;</div></td></td>
+                        </tr>
+                        <tr>
+                            <td class="week-view-day-title">Wed 21</td>
+                            <td class="week-view-day-value">
+                                <div class="week-view-day-value-block" style="width: 20%">&nbsp;</div>
+                                <div>&nbsp; 0h 46m</div>
+                            </td>
+                            <td class="week-view-day-temperatures"><div>8&deg; &nbsp; 13&deg;</div></td></td>
+                        </tr>
+                        <tr>
+                            <td class="week-view-day-title">Tue 20</td>
+                            <td class="week-view-day-value">
+                                <div class="week-view-day-value-block" style="width: 25%">&nbsp;</div>
+                                <div>&nbsp; 0h 56m</div>
+                            </td>
+                            <td class="week-view-day-temperatures"><div>8&deg; &nbsp; 13&deg;</div></td></td>
+                        </tr>
+                        <tr>
+                            <td class="week-view-day-title">Mon 19</td>
+                            <td class="week-view-day-value">
+                                <div class="week-view-day-value-block" style="width: 74%">&nbsp;</div>
+                                <div>&nbsp; 2h 48m</div>
+                            </td>
+                            <td class="week-view-day-temperatures"><div>-2&deg; &nbsp; 3&deg;</div></td></td>
+                        </tr>
+                        <tr>
+                            <td class="week-view-day-title">Sun 18</td>
+                            <td class="week-view-day-value">
+                                <div class="week-view-day-value-block" style="width: 66%">&nbsp;</div>
+                                <div>&nbsp; 2h 34m</div>
+                            </td>
+                            <td class="week-view-day-temperatures"><div>-2&deg; &nbsp; 8&deg;</div></td></td>
+                        </tr>
+                        <tr>
+                            <td class="week-view-day-title">Sat 17</td>
+                            <td class="week-view-day-value">
+                                <div class="week-view-day-value-block" style="width: 64%">&nbsp;</div>
+                                <div>&nbsp; 2h 24m</div>
+                            </td>
+                            <td class="week-view-day-temperatures"><div>-2&deg; &nbsp; 9&deg;</div></td></td>
+                        </tr>
+                    </table>
+                    <textarea>
+                        ${sensorInformation}
+                    </textarea>
                 </div>
-            </ha-card>
+            </div>
         `;
-    
-    }
+	}
 }
 
 (window as any).customCards = (window as any).customCards || [];
 (window as any).customCards.push({
-    type: 'hive-heating-stats-card',
-    name: 'Hive Heating Stats Card',
-    preview: false, // Optional - defaults to false
-    description: "Hive Heating"
+	type: 'hive-heating-stats-card',
+	name: 'Hive Heating Stats Card',
+	preview: false, // Optional - defaults to false
+	description: 'Hive Heating',
 });
--- src/style.ts
+++ src/style.ts
@@ -1,24 +1,104 @@
-import { CSSResultGroup, css } from "lit";
+import { CSSResultGroup, css } from 'lit';
 
 export const styles: CSSResultGroup = css`
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100%;
-        width: 100%;
-        padding: 5px;
-    }
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+		padding: 5px;
+	}
 
-    .card {
-        border-radius: var(--ha-card-border-radius, 10px);
-        box-shadow: var(--ha-card-box-shadow, 0px 0px 0px 1px rgba(0, 0, 0, 0.12), 0px 0px 0px 0px rgba(0, 0, 0, 0.12), 0px 0px 0px 0px rgba(0, 0, 0, 0.12));
-        background: var(--ha-card-background, var(--card-background-color, white));
-        border-width: var(--ha-card-border-width);
-        padding: 0px;
-    }
+	.card {
+		border-radius: var(--ha-card-border-radius, 10px);
+		box-shadow: var(
+			--ha-card-box-shadow,
+			0px 0px 0px 1px rgba(0, 0, 0, 0.12),
+			0px 0px 0px 0px rgba(0, 0, 0, 0.12),
+			0px 0px 0px 0px rgba(0, 0, 0, 0.12)
+		);
+		background: var(--ha-card-background, var(--card-background-color, white));
+		border-width: var(--ha-card-border-width);
+		padding: 0px;
+	}
 
-    text { text-anchor: middle; alignment-baseline: middle; }
+	text {
+		text-anchor: middle;
+		alignment-baseline: middle;
+	}
 
-    `;
+	h1 {
+		text-align: center;
+	}
+
+	.grey-box {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 80%;
+		padding: 20px;
+		background-color: rgb(228, 228, 228);
+		border-radius: 10px;
+	}
+
+	.grey-box-half {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 50%;
+		padding: 5px;
+		background-color: rgb(228, 228, 228);
+	}
+
+	.grey-box-half:first-of-type {
+		border-right: 1px solid black;
+	}
+
+	.grey-box-half:last-of-type {
+		border-left: 1px solid black;
+	}
+
+	.grey-box-units span {
+		font-size: 2.8em;
+	}
+
+	.week-view {
+		width: 80%;
+	}
+
+	table {
+		border-collapse: inherit;
+		border-spacing: 0 10px;
+	}
+
+	.week-view-day-title {
+		width: 15%;
+	}
+
+	.week-view.day-value {
+		width: 70%;
+	}
+
+	.week-view-day-temperatures {
+		width: 15%;
+	}
+
+	.week-view-day-temperatures div {
+		text-align: center;
+		background-color: rgb(228, 228, 228);
+		border-radius: 10px;
+	}
+
+	.week-view-day-value-block {
+		height: 100%;
+		background-color: rgb(71, 71, 252);
+		border-radius: 10px;
+		float: left;
+	}
+`;
